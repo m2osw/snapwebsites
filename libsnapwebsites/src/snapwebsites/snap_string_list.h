2f8173661019f45269f5862dc5e343e2d7d0173b
AlexisWilke <alexis@m2osw.com>
2019-07-21 03:04:08 -0700

    Added a #include <vector> in the snap_string_list.cpp for 19.04.

--- libsnapwebsites/src/snapwebsites/snap_string_list.h
+++ libsnapwebsites/src/snapwebsites/snap_string_list.h
@@ -16,9 +16,20 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #pragma once
 
-#include <QStringList>
 
 #include "snapwebsites/snap_exception.h"
+
+
+// Qt lib
+//
+#include <QStringList>
+
+
+// C++ lib
+//
+#include <vector>
+
+
 
 namespace snap
 {
